3dceb9ebcfd1a449b9a6d0d26ba96bfd9df605ab
Michael Thierry <michaelthierry86@gmail.com>
2022-05-05 21:11:26 -0300

    ok

---
+++ Exercicios/Main.java
@@ -0,0 +1,5 @@
+public class Main{
+    public static void main(String[] args) {
+        Thread s1 = new Thread();
+    }
+}
---
+++ Exercicios/Semaforo.java
@@ -0,0 +1,5 @@
+public class Semaforo implements Runnable{
+        public void run(){
+            System.out.println("Semaforo");
+        }
+}
