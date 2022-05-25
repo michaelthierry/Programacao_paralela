86e8ddf618e18d113622854180916e8594451364
Michael Thierry <michaelthierry86@gmail.com>
2022-05-25 00:24:22 -0300

    ok

--- Exemplos/Ex00/Main.java
+++ Exemplos/Ex00/Main.java
@@ -1,18 +1,13 @@
 /**
- * Testando minha thread
+ * Criando Threads: 
+ * - Extendendo de Threads
+ *  
  */
 public class Main {
     public static void main(String[] args) {
-        /*Thread com extends de Thread
-        MinhaThread minhathread = new MinhaThread(); //criando minha thread
-        minhathread.start(); // inicializando minha thread
-        */
+        //Thread com extends de Thread
+        MinhaThread minhathread = new MinhaThread("1");
+        minhathread.start();
         
-        //Thread com implements runnable
-        MinhaThreadRunable minhaThread = new MinhaThreadRunable(); //criando um objeto runable
-        Thread t = new Thread(minhaThread); // passando meu runable para uma thread
-        t.start(); //Rodando minha thread.
-        //*/
-
     }
 }
--- Exemplos/Ex00/MinhaThread.java
+++ Exemplos/Ex00/MinhaThread.java
@@ -1,6 +1,17 @@
-/**Apartir de extends */
+/**
+ * Criando uma classe que extends de Thread 
+ */
 public class MinhaThread extends Thread{
+    //Atributos de minhaThread
+    private String nome;
+
+    //Construtor
+    public MinhaThread(String nome){
+        this.nome = nome;
+    }
+
+    //Implementando o metodo run()
     public void run(){
-        System.out.println("Olá");
+        System.out.println(this.nome + " Thread executando");
     }
 }
